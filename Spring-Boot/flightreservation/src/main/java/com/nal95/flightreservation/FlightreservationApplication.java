702e73c3ee8017e30d0f2f6a8daef65f2c5b1f69
nal95 <alex.nguepi@yahoo.fr>
2022-09-21 14:00:03 +0200

    java dependency's init

--- Spring-Boot/flightreservation/src/main/java/com/nal95/flightreservation/FlightreservationApplication.java
+++ Spring-Boot/flightreservation/src/main/java/com/nal95/flightreservation/FlightreservationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlightreservationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlightreservationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlightreservationApplication.class, args);
+    }
 
 }
