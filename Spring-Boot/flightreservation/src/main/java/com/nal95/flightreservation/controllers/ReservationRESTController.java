493352b0b556e9a4a78c01038030a42083a74909
nal95 <alex.nguepi@yahoo.fr>
2022-09-27 11:14:37 +0200

    reservation data transfer object (dto) and use case

---
+++ Spring-Boot/flightreservation/src/main/java/com/nal95/flightreservation/controllers/ReservationRESTController.java
@@ -0,0 +1,30 @@
+package com.nal95.flightreservation.controllers;
+
+import com.nal95.flightreservation.dto.ReservationUpdateRequest;
+import com.nal95.flightreservation.entities.Reservation;
+import com.nal95.flightreservation.repos.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+public class ReservationRESTController {
+
+    @Autowired
+    ReservationRepository reservationRepository;
+
+    @RequestMapping("/reservation/{id}")
+    public Reservation findReservation(@PathVariable("id") Long id){
+        return reservationRepository.findById(id).orElseThrow();
+
+    }
+
+    @RequestMapping("reservations")
+    public Reservation updateReservation(@RequestBody ReservationUpdateRequest request){
+        Reservation reservation = reservationRepository.findById(request.getId()).orElseThrow();
+        reservation.setNumberOfBags(request.getNumberOfBags());
+        reservation.setCheckedIn(request.isCheckedIn());
+        return reservationRepository.save(reservation);
+
+    }
+}
