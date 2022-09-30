f45384096b1df052cc8f3325996a7615d5640247
nal95 <alex.nguepi@yahoo.fr>
2022-09-30 13:37:57 +0200

    clean code

--- Spring-Boot/flightreservation/src/main/java/com/nal95/flightreservation/repos/services/SecurityServiceImpl.java
+++ Spring-Boot/flightreservation/src/main/java/com/nal95/flightreservation/repos/services/SecurityServiceImpl.java
@@ -1,6 +1,5 @@
 package com.nal95.flightreservation.repos.services;
 
-import com.nal95.flightreservation.controllers.UserController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
