bb60925d34456c52e427cfd2f276cf36ce08c174
nal95 <alex.nguepi@yahoo.fr>
2022-09-30 13:40:45 +0200

    transaction management (atomic unit of work)

--- Spring-Boot/flightreservation/src/main/java/com/nal95/flightreservation/repos/services/ReservationServiceImpl.java
+++ Spring-Boot/flightreservation/src/main/java/com/nal95/flightreservation/repos/services/ReservationServiceImpl.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReservationServiceImpl implements ReservationService{
@@ -35,6 +36,7 @@ public class ReservationServiceImpl implements ReservationService{
     private static final Logger LOGGER = LoggerFactory.getLogger(ReservationServiceImpl.class);
 
     @Override
+    @Transactional
     public Reservation bookFlight(ReservationRequest request){
 
         LOGGER.info("Inside bookFlight()");
