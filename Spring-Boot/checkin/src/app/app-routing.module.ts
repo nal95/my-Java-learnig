8e7207d67912fd7d236d89f3ed76c7afbd73ccfc
nal95 <alex.nguepi@yahoo.fr>
2022-09-30 15:38:43 +0200

    routing

--- Spring-Boot/checkin/src/app/app-routing.module.ts
+++ Spring-Boot/checkin/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ReservationComponent} from "./components/reservation/reservation.component";
+import {ReservationInfosComponent} from "./components/reservation-infos/reservation-infos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo:'',pathMatch:'full'},
+  {
+    path:'startCheckIn',
+    component: HomeComponent
+  },
+  {
+    path:'checkIn',
+    component: ReservationComponent
+  },
+  {
+    path:'confirm',
+    component: ReservationInfosComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
