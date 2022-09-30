d09544280920d6aefc6f1ba4f1cd3e2c0ddc9335
nal95 <alex.nguepi@yahoo.fr>
2022-09-30 15:40:31 +0200

    checkin service

--- Spring-Boot/checkin/src/app/services/checkin.service.ts
+++ Spring-Boot/checkin/src/app/services/checkin.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from "@angular/common/http";
+import { Reservation } from "../models/reservation"
+import {Observable} from "rxjs";
+import { Checkin } from "../models/checkin"
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckinService {
 
-  constructor() { }
+  reservationUrl = "http://localhost:8080/flightreservation/reservations"
+  constructor( private httpClient:HttpClient) { }
+
+  public getReservation(id:number): Observable<Reservation>{
+    return this.httpClient.get<Reservation>(this.reservationUrl+id);
+  }
+
+  public checkIn(requestBody:Checkin):Observable<number>{
+    return this.httpClient.put<number>(this.reservationUrl,requestBody);
+  }
 }
