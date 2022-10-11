b2e33e4669b809211ed4f2b84c6971eca4ad8d46
nal95 <alex.nguepi@yahoo.fr>
2022-10-11 09:28:53 +0200

    patient controller impl

---
+++ Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/api/PatientController.java
@@ -0,0 +1,35 @@
+package com.nal95.clinic.api;
+
+import com.nal95.clinic.model.Patient;
+import com.nal95.clinic.repos.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class PatientController {
+
+    private final PatientRepository repository;
+
+    @Autowired
+    PatientController(PatientRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping(value = "/patients")
+    public List<Patient> getPatients(){
+        return repository.findAll();
+    }
+
+    @GetMapping(value = "/patients/{id}")
+    public Patient getPatient(@PathVariable("id") int id){
+        return repository.findById(id).orElseThrow();
+    }
+
+    @PostMapping(value = "/patients")
+    public Patient savePatients(Patient patient){
+        return repository.save(patient);
+    }
+}
