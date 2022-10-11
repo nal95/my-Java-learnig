c22184d1357799102109189c349b691214097cde
nal95 <alex.nguepi@yahoo.fr>
2022-10-11 08:42:14 +0200

    project clinic init

---
+++ Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/ClinicalserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nal95.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicalserviceApplication.class, args);
+    }
+
+}
---
+++ Spring-Boot/Clinic/backend/clinicalservice/src/test/java/com/nal95/clinic/ClinicalserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nal95.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
