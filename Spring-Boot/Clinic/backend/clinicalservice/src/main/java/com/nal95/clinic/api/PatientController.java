e51b63d897ae3931d002729865aeb924ea4e6896
nal95 <alex.nguepi@yahoo.fr>
2022-10-11 19:13:01 +0200

    get patient analyse

--- Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/api/PatientController.java
+++ Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/api/PatientController.java
@@ -1,17 +1,24 @@
 package com.nal95.clinic.api;
 
+import com.nal95.clinic.model.ClinicalData;
 import com.nal95.clinic.model.Patient;
 import com.nal95.clinic.repos.PatientRepository;
+import com.nal95.clinic.util.BMICalculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class PatientController {
 
     private final PatientRepository repository;
+    Map<String, String> filtersEntries = new HashMap<>();
 
     @Autowired
     PatientController(PatientRepository repository){
@@ -31,5 +38,22 @@ public class PatientController {
     @PostMapping(value = "/patients")
     public Patient savePatients(@RequestBody Patient patient){
         return repository.save(patient);
+    }
+    @GetMapping(value="/patients/analyze/{id}")
+    public Patient analyze(@PathVariable("id") int id){
+        Patient patient = repository.findById(id).orElseThrow();
+        List<ClinicalData> clinicalData = patient.getClinicalData();
+        List<ClinicalData> duplicateClinicalData = new ArrayList<>(clinicalData);
+        for(ClinicalData eachEntry:duplicateClinicalData){
+            if(filtersEntries.containsKey(eachEntry.getComponentName())){
+                clinicalData.remove(eachEntry);
+                continue;
+            }else {
+                filtersEntries.put(eachEntry.getComponentName(),null);
+            }
+            BMICalculator.calculateBMI(clinicalData,eachEntry);
+        }
+        filtersEntries.clear();
+        return patient;
     }
 }
