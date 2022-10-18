a46cd99d27ffcefb3b9d488a789cc299a34ca5cf
nal95 <alex.nguepi@yahoo.fr>
2022-10-18 12:36:16 +0200

    security webSecurityConfig v2

--- Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/security/config/WebSecurityConfig.java
+++ Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/security/config/WebSecurityConfig.java
@@ -1,10 +1,10 @@
 package com.nal95.clinic.security.config;
 
+import com.nal95.clinic.services.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,22 +14,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    protected   BCryptPasswordEncoder bCryptPasswordEncoder(){
-        return new BCryptPasswordEncoder();
+    private final UserService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public WebSecurityConfig (UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder){
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST,"/api/users")
-                .permitAll().anyRequest().authenticated();
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
+
+        //Configure AuthenticationManagerBuilder
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/api/users").permitAll()
+                .anyRequest().authenticated();
+
         return http.build();
-    }
-
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 }
