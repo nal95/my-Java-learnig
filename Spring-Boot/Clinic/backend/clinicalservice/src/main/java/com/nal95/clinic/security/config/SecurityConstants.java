b1b6701a892ef1ac3e86ee7f4c8ff4bb52ef4738
nal95 <alex.nguepi@yahoo.fr>
2022-10-18 18:40:31 +0200

    const for wedSecurity management

---
+++ Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/security/config/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.nal95.clinic.security.config;
+
+public class SecurityConstants {
+    public static final long EXPIRATION_TIME = 864000000; //10 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "/api/users";
+    public static final String TOKEN_SECRET = "jf9i4jgu83nfl0l";
+}
