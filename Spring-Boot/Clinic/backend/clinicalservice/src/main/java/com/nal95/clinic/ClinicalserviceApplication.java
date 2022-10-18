42119800c0d91ce9ac28daae4d95a3f63ab261fb
nal95 <alex.nguepi@yahoo.fr>
2022-10-18 12:34:13 +0200

    to encode the pwd

--- Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/ClinicalserviceApplication.java
+++ Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/ClinicalserviceApplication.java
@@ -2,12 +2,19 @@ package com.nal95.clinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ClinicalserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClinicalserviceApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
