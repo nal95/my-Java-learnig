d3baf353761d16d675b3e6ed32d81114a9c0a831
nal95 <alex.nguepi@yahoo.fr>
2022-10-19 17:13:19 +0200

    clean code

--- Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/api/UserController.java
+++ Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/api/UserController.java
@@ -4,11 +4,7 @@ import com.nal95.clinic.dto.UserDto;
 import com.nal95.clinic.model.User;
 import com.nal95.clinic.model.responses.UserRest;
 import com.nal95.clinic.services.UserService;
-import com.nal95.clinic.util.UuidUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -19,7 +15,6 @@ public class UserController {
     final
     UserService userService;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
