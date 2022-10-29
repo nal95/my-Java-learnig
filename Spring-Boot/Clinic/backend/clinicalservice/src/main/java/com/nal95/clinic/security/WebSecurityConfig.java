00fad8a197160f738f8a6aea842017f197640ea9
nal95 <alex.nguepi@yahoo.fr>
2022-10-29 02:02:58 +0200

    webconfig CustomAuthenticationFilter

--- Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/security/WebSecurityConfig.java
+++ Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/security/WebSecurityConfig.java
@@ -1,23 +1,31 @@
 package com.nal95.clinic.security;
 
-import lombok.RequiredArgsConstructor;
+import com.nal95.clinic.services.UserService;
+import com.nal95.clinic.utils.AppRouteConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    private final UserDetailsService userDetailsService;
+    private final UserService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public WebSecurityConfig (UserService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder){
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
@@ -26,21 +34,17 @@ public class WebSecurityConfig {
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
 
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+        http.authenticationManager(authenticationManager);
 
+        final CustomAuthenticationFilter filter = new CustomAuthenticationFilter(authenticationManager);
+        filter.setFilterProcessesUrl(AppRouteConstants.SIGN_IN_URL);
 
         http.csrf().disable().authorizeRequests();
 
         return http.build();
     }
 
-/*    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }*/
-
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 }
