38d0362fbf01a1f4a4d878a74c2da84ec8110064
nal95 <alex.nguepi@yahoo.fr>
2022-10-31 01:49:19 +0100

    mail sending (Async)

--- Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/services/MailServiceIpml.java
+++ Spring-Boot/Clinic/backend/clinicalservice/src/main/java/com/nal95/clinic/services/MailServiceIpml.java
@@ -9,6 +9,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,6 +21,7 @@ public class MailServiceIpml implements MailService{
     private final MailContentBuilder mailContentBuilder;
 
     @Override
+    @Async
     public void sendMail(NotificationEmail notificationEmail, String token) {
 
         MimeMessagePreparator messagePreparator = mimeMessage -> {
