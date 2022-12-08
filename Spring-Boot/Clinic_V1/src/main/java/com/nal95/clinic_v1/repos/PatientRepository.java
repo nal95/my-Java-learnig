a0b73d89a0a42e6b3c892a61f238bb18c5ea2cb1
nal95 <alex.nguepi@yahoo.fr>
2022-12-08 03:54:22 +0100

    clinical app  structure

--- Spring-Boot/Clinic_V1/src/main/java/com/nal95/clinic_v1/repos/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nal95.clinic_v1.repos;
-
-import com.nal95.clinic_v1.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
